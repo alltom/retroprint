2e9064232ebbba5da4a4acc54630ca6f99a8bb17
Tom Lieber <tom@alltom.com>
2016-09-16 19:03:28 -0700

    only show entries for the variable under the cursor

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var CodeMirror = require('codemirror');
 require('codemirror/lib/codemirror.css');
 require('codemirror/mode/javascript/javascript');
@@ -11,5 +12,16 @@ var outputContainer = document.createElement('pre');
 document.body.appendChild(outputContainer);
 
 eval(instrument(src));
-outputContainer.appendChild(
-    document.createTextNode(JSON.stringify(SAVED_VALUES, null, '  ')));
+
+editor.on('cursorActivity', function() {
+  var loc = editor.indexFromPos(editor.getCursor());
+  _.chain(SAVED_VALUES)
+      .filter(function(entry) {
+        return entry.start <= loc && entry.end >= loc;
+      })
+      .tap(function(entries) {
+        outputContainer.innerHTML = '';
+        outputContainer.appendChild(
+            document.createTextNode(JSON.stringify(entries, null, '  ')));
+      });
+});
