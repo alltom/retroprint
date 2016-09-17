04cb73b5fd566e9930422700aafb6153754ef9a6
Tom Lieber <tom@alltom.com>
2016-09-16 18:50:44 -0700

    show the code and output on the web page

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var CodeMirror = require('codemirror');
+require('codemirror/lib/codemirror.css');
+require('codemirror/mode/javascript/javascript');
 var instrument = require('./instrument');
 
 var src = require('raw!./example01.js');
 
+var editor =
+    CodeMirror(document.body, {value: src, mode: 'javascript', readOnly: true});
+var outputContainer = document.createElement('pre');
+document.body.appendChild(outputContainer);
+
 eval(instrument(src));
-console.log(JSON.stringify(SAVED_VALUES, null, '  '));
+outputContainer.appendChild(
+    document.createTextNode(JSON.stringify(SAVED_VALUES, null, '  ')));
